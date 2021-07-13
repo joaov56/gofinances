7706b7b12ee22dc6e029d234c2f68e1ee32b0a01
João <vitorjao56@gmail.com>
2021-07-13 11:46:21 -0300

    feat: add header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-
-import { Container, Title } from './styles'
+import { 
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper ,
+    Icon
+} from './styles'
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/61507139?v=4' }}/>
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>João</UserName>
+                        </User>
+                    </UserInfo>
+                <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
