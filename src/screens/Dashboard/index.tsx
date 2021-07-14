40399cadd0e0ee049fc667655666573419fa4c88
João <vitorjao56@gmail.com>
2021-07-14 12:11:24 -0300

    create list of transactions and change types

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 import { 
     Container,
     Header,
@@ -10,10 +13,51 @@ import {
     UserName,
     UserWrapper ,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
 export function Dashboard() {
+    const data:DataListProps[] = [
+        {
+        id: '1',
+        type: 'positive',
+        title: "Desenvolvimento de site",
+        amount: "R$12.000,00",
+        category: {
+                name: 'Vendas',
+            icon: 'dollar-sign',
+        },
+        date: "13/04/2021"
+    },
+    {   
+        id: '2',
+        type: 'negative',
+        title: "Varietá",
+        amount: "R$150,00",
+        category: {
+            name: 'Alimentação',
+            icon: 'coffee',
+        },
+        date: "13/07/2021"
+    },
+    {   
+        id: '3',
+        type: 'negative',
+        title: "Alugel",
+        amount: "R$1.200,00",
+        category: {
+            name: 'Casa',
+            icon: 'shopping-bag',
+        },
+        date: "13/07/2021"
+    }
+    ]   
     return (
         <Container>
             
@@ -50,6 +94,14 @@ export function Dashboard() {
                 /> 
             </HighlightCards>
             
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionList 
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({item})=> <TransactionCard data={item} />}
+                />
+            </Transactions>
         </Container>
     )
 }
